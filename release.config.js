804e37350478127d8cfc7db98d21e86777cb6a04
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-08-04 17:01:39 +0200

    chore(build): add semantic-release config file

---
+++ release.config.js
@@ -0,0 +1,16 @@
+const publishCmd = `
+echo "Publish command here"
+`;
+
+const config = require('semantic-release-preconfigured-conventional-commits');
+config.plugins.push(
+    [
+        "@semantic-release/exec",
+        {
+            "publishCmd": publishCmd,
+        }
+    ],
+    "@semantic-release/github",
+    "@semantic-release/git",
+)
+module.exports = config
